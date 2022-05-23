8d515e1d909ebc37222d7dd3fc49ee12658c78b4
soonkuk.kang <soonkuk@gmail.com>
2022-05-23 11:56:26 +0900

    chore : remove unused type
    remove Config interface and BaseConfigData type

--- extension/contract_account.go
+++ extension/contract_account.go
@@ -2,7 +2,6 @@ package extension // nolint: dupl, revive
 
 import (
 	"github.com/spikeekips/mitum/base"
-	"github.com/spikeekips/mitum/base/state"
 	"github.com/spikeekips/mitum/util"
 	"github.com/spikeekips/mitum/util/hint"
 	"github.com/spikeekips/mitum/util/valuehash"
@@ -83,73 +82,4 @@ func (cs ContractAccount) Equal(b ContractAccount) bool {
 	}
 
 	return true
-}
-
-type Config interface {
-	ID() string            // id of state set in contract model
-	ConfigType() hint.Type // config type in contract model
-	Hint() hint.Hint
-	Bytes() []byte
-	Hash() valuehash.Hash
-	GenerateHash() valuehash.Hash
-	IsValid([]byte) error
-	Address() base.Address // contract account address
-	SetStateValue(st state.State) (state.State, error)
-}
-
-var (
-	BaseConfigDataType   = hint.Type("mitum-currency-contract-account-configdata")
-	BaseConfigDataHint   = hint.NewHint(BaseConfigDataType, "v0.0.1")
-	BaseConfigDataHinter = BaseConfigData{BaseHinter: hint.NewBaseHinter(BaseConfigDataHint)}
-)
-
-type BaseConfigData struct {
-	hint.BaseHinter
-	config Config
-}
-
-func NewBaseConfigData(cfg Config) (BaseConfigData, error) {
-	err := cfg.IsValid(nil)
-	if err != nil {
-		return BaseConfigData{}, err
-	}
-	bcfg := BaseConfigData{
-		BaseHinter: hint.NewBaseHinter(BaseConfigDataHint),
-		config:     cfg,
-	}
-	return bcfg, nil
-}
-
-func (cfd BaseConfigData) Config() Config {
-	return cfd.config
-}
-
-func (cfd BaseConfigData) SetConfig(cfg Config) (BaseConfigData, error) {
-	err := cfg.IsValid(nil)
-	if err != nil {
-		return BaseConfigData{}, err
-	}
-
-	cfd.config = cfg
-	return cfd, nil
-}
-
-func (cfd BaseConfigData) Hash() valuehash.Hash {
-	return cfd.GenerateHash()
-}
-
-func (cfd BaseConfigData) GenerateHash() valuehash.Hash {
-	return valuehash.NewSHA256(cfd.Bytes())
-}
-
-func (cfd BaseConfigData) Bytes() []byte {
-	return cfd.config.Bytes()
-}
-
-func (cfd BaseConfigData) IsValid([]byte) error {
-	return cfd.config.IsValid(nil)
-}
-
-func (cfd BaseConfigData) Equal(b BaseConfigData) bool {
-	return cfd.Equal(b)
 }
--- extension/state.go
+++ extension/state.go
@@ -12,8 +12,7 @@ import (
 )
 
 var (
-	StateKeyContractAccountSuffix       = ":contractaccount"
-	StateKeyContractAccountConfigSuffix = ":contractaccountconfig"
+	StateKeyContractAccountSuffix = ":contractaccount"
 )
 
 func StateKeyContractAccount(a base.Address) string {
@@ -38,35 +37,6 @@ func StateContractAccountValue(st state.State) (ContractAccount, error) {
 }
 
 func SetStateContractAccountValue(st state.State, v ContractAccount) (state.State, error) {
-	uv, err := state.NewHintedValue(v)
-	if err != nil {
-		return nil, err
-	}
-	return st.SetValue(uv)
-}
-
-func StateKeyContractAccountConfig( /* model name */ m string, id string, a base.Address) string {
-	return fmt.Sprintf("%s-%s-%s%s", m, id, a.String(), StateKeyContractAccountConfigSuffix)
-}
-
-func IsStateContractAccountConfigKey(key string) bool {
-	return strings.HasSuffix(key, StateKeyContractAccountConfigSuffix)
-}
-
-func StateContractAccountConfigValue(st state.State) (Config, error) {
-	v := st.Value()
-	if v == nil {
-		return nil, util.NotFoundError.Errorf("config not found in State")
-	}
-
-	s, ok := v.Interface().(Config)
-	if !ok {
-		return nil, errors.Errorf("invalid config value found, %T", v.Interface())
-	}
-	return s, nil
-}
-
-func setStateContractAccountConfigValue(st state.State, v Config) (state.State, error) {
 	uv, err := state.NewHintedValue(v)
 	if err != nil {
 		return nil, err
