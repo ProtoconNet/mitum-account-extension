5aeb66107ce2c8e6af6b6347f63865df4fdbb28e
soonkuk <soonkuk@gmail.com>
2023-04-24 18:34:09 +0900

    feat: Add ContractIDFlag to cmds

--- cmds/flags.go
+++ cmds/flags.go
@@ -11,6 +11,7 @@ import (
 	"github.com/ProtoconNet/mitum2/base"
 	"github.com/ProtoconNet/mitum2/util/encoder"
 
+	extensioncurrency "github.com/ProtoconNet/mitum-currency-extension/v2/currency"
 	"github.com/ProtoconNet/mitum-currency/v2/currency"
 )
 
@@ -213,4 +214,22 @@ func (v *CurrencyAmountFlag) UnmarshalText(b []byte) error {
 
 func (v *CurrencyAmountFlag) String() string {
 	return v.CID.String() + "," + v.Big.String()
+}
+
+type ContractIDFlag struct {
+	CID extensioncurrency.ContractID
+}
+
+func (v *ContractIDFlag) UnmarshalText(b []byte) error {
+	cid := extensioncurrency.ContractID(string(b))
+	if err := cid.IsValid(nil); err != nil {
+		return err
+	}
+	v.CID = cid
+
+	return nil
+}
+
+func (v *ContractIDFlag) String() string {
+	return v.CID.String()
 }
